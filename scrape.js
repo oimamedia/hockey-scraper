de6702b07997cea7f07853f4a5d34709b4e934ef
oimamedia <oimamediaholder@gmail.com>
2026-05-31 21:17:35 +0300

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -18,7 +18,7 @@ const context = await browser.newContext({
 const page = await context.newPage();
 
 await page.goto(URL, { waitUntil: "networkidle" });
-await page.waitForFunction(() => typeof ui !== "undefined", { timeout: 15000 });
+await page.waitForTimeout(8000);
 
 const [standingsResponse] = await Promise.all([
   page.waitForResponse(
