2db20ed8e364a19afa476d5616c55c600b8d6cd0
oimamedia <oimamediaholder@gmail.com>
2026-06-05 12:08:21 +0300

    Update scrape-games.js

--- scrape-games.js
+++ scrape-games.js
@@ -7,7 +7,7 @@ chromium.use(StealthPlugin());
 const URL = "https://tulospalvelu.leijonat.fi/serie?lang=fi&season=2026&lid=67&ssid=201";
 
 const browser = await chromium.launch({
-  headless: true,
+  headless: false,
   args: ["--no-sandbox", "--disable-setuid-sandbox"],
 });
 
